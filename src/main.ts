ede3d9f92a26775afdf548e195df4a87e5007ffd
Akshit Batra <akbatra567@gmail.com>
2025-04-05 00:57:33 +0530

    setup swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce')
+    .setDescription('Ecommerce Website')
+    .setVersion('1.0')
+    .addTag('ecommerce')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
