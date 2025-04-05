6f7a6eaa48c77bcb8ca438d15fbdf876f9b3b616
Akshit Batra <akbatra567@gmail.com>
2025-04-05 17:59:02 +0530

    setup(modules) generate modules using nest generator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+import { CartModule } from './cart/cart.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, UserModule, OrderModule, CartModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
